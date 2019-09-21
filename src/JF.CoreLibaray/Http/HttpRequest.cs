2d76d667f3652b693c3384847ce79237a495367d
GLOBALE\wuchaohui <wuchaohui@globalegrow.com>
2019-09-21 17:56:28 +0800

    http请求相关调整

--- src/JF.CoreLibaray/Http/HttpRequest.cs
+++ src/JF.CoreLibaray/Http/HttpRequest.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -12,31 +14,19 @@ namespace JF.Http
     public class HttpRequest
     {
         /// <summary>
-        /// 使用post方法异步请求
+        /// 使用post返回异步请求直接返回对象
         /// </summary>
-        /// <param name="url">目标链接</param>
-        /// <param name="json">发送的参数字符串，只能用json</param>
+        /// <typeparam name="TResult">返回对象类型</typeparam>
+        /// <typeparam name="TData">请求对象类型</typeparam>
+        /// <param name="url">请求链接</param>
+        /// <param name="obj">请求对象数据</param>
         /// <param name="header"></param>
-        /// <returns>返回的字符串</returns>
-        public static async Task<string> PostAsyncJson(string url, string json, Dictionary<string, string> header = null)
+        /// <returns>请求返回的目标对象</returns>
+        public static async Task<TResult> PostAsync<TResult, TData>(string url, TData obj, Dictionary<string, string> header = null)
+            where TResult : class
         {
-            using (var client = new HttpClient())
-            {
-                HttpContent content = new StringContent(json);
-                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-                if (header != null)
-                {
-                    client.DefaultRequestHeaders.Clear();
-                    foreach (var item in header)
-                    {
-                        client.DefaultRequestHeaders.Add(item.Key, item.Value);
-                    }
-                }
-                HttpResponseMessage response = await client.PostAsync(url, content);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
+            String json = JsonConvert.SerializeObject(obj);
+            return await PostJsonAsync<TResult>(url, json, header);
         }
 
         /// <summary>
@@ -45,23 +35,54 @@ namespace JF.Http
         /// <param name="url">目标链接</param>
         /// <param name="data">发送的参数字符串</param>
         /// <returns>返回的字符串</returns>
-        public static async Task<string> PostAsync(string url, string data = null, Dictionary<string, string> header = null)
+        public static async Task<string> PostStringAsync(string url, string data = null, Dictionary<string, string> header = null)
         {
-            using (var client = new HttpClient(new HttpClientHandler() { UseCookies = false }))
+            using (var client = new HttpClient(new HttpClientHandler() { UseProxy = false }))
             {
-                HttpContent content = data != null ? new StringContent(data) : null;
                 if (header != null)
                 {
-                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Clear();
                     foreach (var item in header)
                     {
                         client.DefaultRequestHeaders.Add(item.Key, item.Value);
                     }
                 }
+
+                HttpContent content = data != null ? new StringContent(data) : null;
+
                 HttpResponseMessage response = await client.PostAsync(url, content);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
+
+                return await resolveResponseResultAsync<string>(response);
+            }
+        }
+
+        /// <summary>
+        /// 使用post方法异步请求
+        /// </summary>
+        /// <param name="url">目标链接</param>
+        /// <param name="json">发送的参数字符串，只能用json</param>
+        /// <param name="header"></param>
+        /// <returns>返回指定的数据类型</returns>
+        public static async Task<TResult> PostJsonAsync<TResult>(string url, string json, Dictionary<string, string> header = null)
+        {
+            using (var client = new HttpClient(new HttpClientHandler() { UseProxy = false }))
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                if (header != null)
+                {
+                    foreach (var item in header)
+                    {
+                        client.DefaultRequestHeaders.Add(item.Key, item.Value);
+                    }
+                }
+
+                HttpContent content = new StringContent(json);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                return await resolveResponseResultAsync<TResult>(response);
             }
         }
 
@@ -72,7 +93,7 @@ namespace JF.Http
         /// <returns>返回的字符串</returns>
         public static async Task<string> GetAsync(string url, Dictionary<string, string> header = null)
         {
-            using (var client = new HttpClient(new HttpClientHandler() { UseCookies = false }))
+            using (var client = new HttpClient(new HttpClientHandler() { UseProxy = false }))
             {
                 if (header != null)
                 {
@@ -82,27 +103,11 @@ namespace JF.Http
                         client.DefaultRequestHeaders.Add(item.Key, item.Value);
                     }
                 }
-                HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();//用来抛异常的
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-        }
 
-        /// <summary>
-        /// 使用post返回异步请求直接返回对象
-        /// </summary>
-        /// <typeparam name="T">返回对象类型</typeparam>
-        /// <typeparam name="T2">请求对象类型</typeparam>
-        /// <param name="url">请求链接</param>
-        /// <param name="obj">请求对象数据</param>
-        /// <param name="header"></param>
-        /// <returns>请求返回的目标对象</returns>
-        public static async Task<T> PostObjectAsync<T, T2>(string url, T2 obj, Dictionary<string, string> header = null)
-        {
-            String json = JsonConvert.SerializeObject(obj);
-            string responseBody = await PostAsyncJson(url, json, header); //请求当前账户的信息
-            return JsonConvert.DeserializeObject<T>(responseBody);//把收到的字符串序列化
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                return await resolveResponseResultAsync<string>(response);
+            }
         }
 
         /// <summary>
@@ -120,98 +125,179 @@ namespace JF.Http
         /// <summary>
         /// 模拟表单请求
         /// </summary>
-        /// <typeparam name="TForm">表单对象</typeparam>
+        /// <typeparam name="TForm"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
         /// <param name="url"></param>
-        /// <param name="value"></param>
+        /// <param name="formData"></param>
         /// <param name="headers"></param>
         /// <returns></returns>
-        public static async Task<HttpResponseMessage> PostByFormAsync<TForm>(string url, TForm value, Dictionary<string, string> headers = null)
+        public static async Task<TResult> PostByFormAsync<TForm, TResult>(string url, TForm formData, Dictionary<string, string> headers = null)
             where TForm : class, new()
         {
-            var modelType = typeof(TForm);
-            using (var client = new HttpClient())
-            using (var formData = new MultipartFormDataContent())
+            var response = await PostByFormAsync(url, formData, headers);
+
+            return await resolveResponseResultAsync<TResult>(response);
+        }
+
+        /// <summary>
+        /// 模拟表单请求
+        /// </summary>
+        /// <typeparam name="TForm">表单对象</typeparam>
+        /// <param name="url"></param>
+        /// <param name="formData"></param>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        public static async Task<HttpResponseMessage> PostByFormAsync<TForm>(string url, TForm formData, Dictionary<string, string> headers = null)
+            where TForm : class, new()
+        {
+            var modelType = formData.GetType();
+            using (var client = new HttpClient(new HttpClientHandler() { UseProxy = false }))
             {
-                #region Header处理
+                #region HttpClient处理
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 if (headers != null)
                 {
-                    client.DefaultRequestHeaders.Clear();
                     foreach (var item in headers)
                     {
                         client.DefaultRequestHeaders.Add(item.Key, item.Value);
                     }
                 }
+
                 #endregion
 
-                //遍历SendData的所有成员
-                foreach (var item in modelType.GetProperties())
-                {
-                    HttpContent content;
-                    #region //文件的处理（这里需要特别注意，流只能读取一次，因为读取之后会把Stream.Positon(当前流中的位置)置为最后读取的位置，除非置为0，第二次才可读到）
-                    if (item.PropertyType == typeof(IFormFile) && item.GetValue(value) != null)
-                    {
-                        #region Stream请求
-                        //Stream塞进Content会会导致读取这个流，所以之后不能再第二次利用
-                        //var model = (IFormFile)item.GetValue(value);
-                        //content = new StreamContent(model.OpenReadStream());//, model.ContentLength);
-                        //content.Headers.ContentType = MediaTypeHeaderValue.Parse(model.ContentType);//ContentType必须赋值,否则文件接收不到此属性
-                        //content.Headers.ContentLength = model.Length;//ContentLength可不显式的赋值,会自动读取给StreamContent的内容长度
-                        //formData.Add(content, item.Name, model.FileName);//文件类型,第三个参数必须要赋值,否则不认为这是一个文件
-                        #endregion
+                #region  构建 MultipartFormDataContent
 
+                var formDataContent = new MultipartFormDataContent();
+                tryBuildMultipartFormDataContent(formData, ref formDataContent);
+
+                #endregion
+
+                return await client.PostAsync(url, formDataContent);
+            }
+        }
+
+        /// <summary>
+        /// 构建MultipartFormDataContent
+        /// </summary>
+        /// <typeparam name="TData"></typeparam>
+        /// <param name="formData"></param>
+        /// <param name="formDataContent"></param>
+        /// <param name="paramName"></param>
+        /// <returns></returns>
+        private static bool tryBuildMultipartFormDataContent<TData>(TData formData, ref MultipartFormDataContent formDataContent, string paramName = null)
+            where TData : class
+        {
+            if (formData == null) return false;
+
+            try
+            {
+                formDataContent = formDataContent ?? new MultipartFormDataContent();
+                var dic = new Dictionary<string, string>();
+                var properties = formData.GetType().GetProperties();
+
+                foreach (var pro in properties)
+                {
+                    var proName = string.IsNullOrEmpty(paramName) ? pro.Name : $"{paramName}.{pro.Name}";
+                    var proValue = pro.GetValue(formData);
+                    if (proValue == null) continue;
+
+                    HttpContent content = null;
+                    #region //文件的处理（这里需要特别注意，流只能读取一次，因为读取之后会把Stream.Positon(当前流中的位置)置为最后读取的位置，除非置为0，第二次才可读到）
+                    if (pro.PropertyType == typeof(IFormFile))
+                    {
                         #region 字节方式请求
-                        var model = (IFormFile)item.GetValue(value);
+                        var model = (IFormFile)proValue;
                         MemoryStream fileTarget = new MemoryStream();
                         model.OpenReadStream().CopyTo(fileTarget);
                         content = new ByteArrayContent(fileTarget.ToArray());
                         content.Headers.ContentType = MediaTypeHeaderValue.Parse(model.ContentType);
                         content.Headers.ContentLength = model.Length;
-                        formData.Add(content, item.Name, model.FileName);
+                        formDataContent.Add(content, proName, model.FileName);
 
                         #endregion
                     }
                     #endregion
                     #region //文件集合的处理
-                    else if (item.PropertyType == typeof(IEnumerable<IFormFile>) && item.GetValue(value) != null)
+                    else if (pro.PropertyType.GetInterfaces().Any(t => t == typeof(IEnumerable<IFormFile>)))
                     {
-                        foreach (var child in ((IEnumerable<IFormFile>)item.GetValue(value)))
+                        foreach (var child in ((IEnumerable<IFormFile>)pro.GetValue(formData)))
                         {
-                            #region Stream请求
-                            //content = new StreamContent(child.OpenReadStream());//, child.Length);
-                            //content.Headers.ContentType = MediaTypeHeaderValue.Parse(child.ContentType);
-                            //content.Headers.ContentLength = child.Length;
-                            //formData.Add(content, item.Name, child.FileName);
-                            #endregion
-
                             #region 字节方式请求
                             MemoryStream fileTarget = new MemoryStream();
                             child.OpenReadStream().CopyTo(fileTarget);
                             content = new ByteArrayContent(fileTarget.ToArray());
                             content.Headers.ContentType = MediaTypeHeaderValue.Parse(child.ContentType);
                             content.Headers.ContentLength = child.Length;
-                            formData.Add(content, item.Name, child.FileName);
+                            formDataContent.Add(content, proName, child.FileName);
                             #endregion
                         }
                     }
                     #endregion
                     #region //如果执意响应方是接收字节类型,那传输时不能用ByteArrayContent去填充,否则接收方认为这是一个非法数据,故要传base64格式,接收方会自动把base64转成字节接收
-                    else if (item.PropertyType == typeof(byte[]) && item.GetValue(value) != null)
+                    else if (pro.PropertyType == typeof(byte[]))
                     {
-                        content = new StringContent(Convert.ToBase64String((byte[])item.GetValue(value)));
-                        formData.Add(content, item.Name);
+                        content = new StringContent(Convert.ToBase64String((byte[])pro.GetValue(formData)));
+                        formDataContent.Add(content, proName);
                     }
                     #endregion
-                    #region //其他类型统一按字符串处理(DateTime,Enum;long ;bool;int...)
-                    else if (item.GetValue(value) != null && (item.PropertyType != typeof(byte[]) || item.PropertyType != typeof(IFormFile)))
+                    #region // 如果为可枚举集合
+                    else if (pro.PropertyType != typeof(String) && pro.PropertyType.GetInterfaces().Any(t => t == typeof(System.Collections.IEnumerable)))
                     {
-                        content = new StringContent(((string)item.GetValue(value).ToString()));
-                        formData.Add(content, item.Name);
+                        var list = pro.GetValue(formData) as IEnumerable<object>;
+                        var index = 0;
+
+                        foreach (var item in list)
+                        {
+                            tryBuildMultipartFormDataContent(item, ref formDataContent, $"{proName}[{index++}]");
+                        }
+                    }
+                    #endregion
+                    #region // 非原生类型
+                    else if (pro.PropertyType != typeof(String) && !pro.PropertyType.IsPrimitive)
+                    {
+                        tryBuildMultipartFormDataContent(pro.GetValue(formData), ref formDataContent, proName);
+                    }
+                    #endregion
+                    #region //其他类型统一按字符串处理
+                    else
+                    {
+                        content = new StringContent(((string)pro.GetValue(formData).ToString()));
+                        formDataContent.Add(content, proName);
                     }
                     #endregion
                 }
 
-                return await client.PostAsync(url, formData);
             }
+            catch
+            {
+                // nothing
+            }
+
+            return formDataContent != default(MultipartFormDataContent);
+        }
+
+        /// <summary>
+        /// 将<see cref="HttpResponseMessage"/>信息解析为指定的类型。
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="responseMessage"></param>
+        /// <returns></returns>
+        private static async Task<TResult> resolveResponseResultAsync<TResult>(HttpResponseMessage responseMessage)
+        {
+            if (responseMessage == null) return default(TResult);
+
+            TResult result = default(TResult);
+
+            responseMessage.EnsureSuccessStatusCode();
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string responseBody = await responseMessage.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<TResult>(responseBody);
+            }
+
+            return result;
         }
 
         /// <summary> 模拟表单请求/liuyl/2017/09/1 /// </summary>
---
+++ test/CoreTestProject/HttpUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using JF.Http;
+
+namespace CoreTestProject
+{
+    public class HttpUnitTest
+    {
+        [Fact]
+        public void PostFormTest()
+        {
+            var api = "http://localhost:64355/api/WebSite/AdManager/ChangeBudget";
+            var formData = new
+            {
+                StrucType = 2,
+                MediaId = 0,
+                Items = new List<object> {
+                    new
+                    {
+                        AccountId = "1619699211439765",
+                        Budget = 202.0000,
+                        Id = "23843207039360695",
+                        PreBudget = 200.0
+                    }
+                }
+            };
+            var headers = new Dictionary<string, string>
+            {
+                { "Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiemVuZ3hpMDEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ByaW1hcnlzaWQiOiIzNzciLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxIiwiaG9zdCI6ImxvY2FsaG9zdCIsIm5iZiI6MTU2OTA1MTc4MSwiZXhwIjoxNTY5MDY5NzgxLCJpc3MiOiJBTVMiLCJhdWQiOiJBTVNfV2ViIn0.zbGg6doRcBRvUd8bkFXqEU08Edyrs3X2zZ0giEUuEj0"}
+            };
+
+            var result = JF.Http.HttpRequest.PostByFormAsync<object, object>(api, formData, headers).Result;
+
+            Assert.True(result != null);
+        }
+    }
+}
